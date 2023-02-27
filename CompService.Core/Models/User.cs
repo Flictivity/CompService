def33fac07c9f3e7e5898e8ef73c4240ef94e40e
Flictivity <bulat_sa1@mail.ru>
2023-02-27 18:30:58 +0300

    remove unused user filed

--- CompService.Core/Models/User.cs
+++ CompService.Core/Models/User.cs
@@ -7,6 +7,5 @@
         public string Email { get; set; } = null!;
         public string? Password { get; set; } 
         public string? PhoneNumber { get; set; }
-        public int VerificationId { get; set; }
     }
 }
